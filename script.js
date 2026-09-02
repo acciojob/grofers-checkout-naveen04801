e5530d27bb68685a8e2181392860d2dce2b21bbd
naveen04801 <113514208+naveen04801@users.noreply.github.com>
2026-09-02 14:03:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const price = document.querySelectorAll(".price");
+	let total = 0;
+	for (let p of price) { total += Number(p.textContent);
+						 }
+	console.log(total);
+	const row = document.createElement("tr");
+	const cell = document.createElement("td"); 
+	cell.textContent = total; row.appendChild(cell);
+	document.querySelector("table").appendChild(row)
   
 };
 
